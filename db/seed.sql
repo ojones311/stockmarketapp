76dcbf5a860002bc77569ce5908bf68865469452
Owen Jones <owenthefirst93@gmail.com>
2021-01-24 14:04:45 -0500

    starting model db

--- db/seed.sql
+++ db/seed.sql
@@ -1,26 +1,35 @@
-DROP DATABASE if exists stockapp_db;
+DROP DATABASE IF EXISTS stockapp_db;
 CREATE DATABASE stockapp_db;
 
-\c stockapp_db;
+\c stockapp_db
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR NOT NULL,
+    username VARCHAR UNIQUE,
+    password VARCHAR UNIQUE,
     avatar_url VARCHAR NOT NULL,
     is_deleted BOOLEAN
 );
 
 CREATE TABLE stocks (
     id SERIAL PRIMARY KEY,
-    ticker VARCHAR NOT NULL,
+    symbol VARCHAR NOT NULL,
     company VARCHAR NOT NULL,
-    
     is_deleted BOOLEAN
-)
+);
+
 CREATE TABLE user_stocks (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     stock_id INT REFERENCES stocks(id),
     is_deleted BOOLEAN,
     UNIQUE(user_id, stock_id)
-)
+);
+
+CREATE TABLE user_relations (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    relation_id INT REFERENCES users(id),
+    is_deleted BOOLEAN
+);
+
