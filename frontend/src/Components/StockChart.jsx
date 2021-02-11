f5f12028996313152215d7de17286aafdeefc39d
Owen Jones <owenthefirst93@gmail.com>
2021-02-10 23:35:35 -0500

    chart instance and config

--- frontend/src/Components/StockChart.jsx
+++ frontend/src/Components/StockChart.jsx
@@ -1,14 +1,94 @@
-import React, {useState} from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 import Chart from 'chart.js'
-import axios from 'axios'
+
 
 const StockChart = ({stock, chart}) => {
-    let chartRef = React.createRef();
-    const myLineGraph = new Chart(chartRef, {
-        type: 'line',
-        data: '',
 
-    })
+    let chartRef = useRef(null);
+    const [chartInstance, setChartInstance] = useState(null)
+
+    const getDateLabels = (arr) =>{
+        let solutionArr = []
+        // let dateArr = arr.filter((elem) => elem.date)
+        // for(let i = 0; i < arr.length; i++){
+        //     if(arr[i].date){
+        //         solutionArr.push(arr[i].date)
+        //     }
+        // }
+        // console.log(arr)
+        // console.log(solutionArr)
+        // return dateArr
+     }
+    let chartConfig = {
+        type: 'line',
+        data: {
+            labels: getDateLabels(chart),
+            datasets:[{
+                label:'Dates',
+                data: [12,3,4,5,6,7]
+            }]
+        },
+        // options: {
+        //     scales: {
+        //         xAxes: [
+        //           {
+        //             type: 'time',
+        //             time: {
+        //               unit: 'week'
+        //             }
+        //           }
+        //         ],
+        //         yAxes: [
+        //           {
+        //             ticks: {
+        //               min: 0
+        //             }
+        //           }
+        //         ]
+        //       }
+        // }
+    }
+
+    useEffect (() => {
+        if(chartRef && chartRef.current){
+            const newLineChart = new Chart(chartRef.current, chartConfig)
+            setChartInstance(newLineChart)
+        }
+    },[chartRef])
+
+    const updateChart = (chartIndex, chart) => {
+        chartInstance.data.datasets[chartIndex].data = chart
+        chart.update()
+    }
+    // const myLineGraph = new Chart(chartRef, {
+    //     type: 'line',
+    //     data: {
+    //         labels: ['One', 'Two','Three', 'Four'],
+    //         datasets:[{
+    //             label:'Numbers',
+    //             data: [12,3,4,5,6,7]
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             xAxes: [
+    //               {
+    //                 type: 'time',
+    //                 time: {
+    //                   unit: 'week'
+    //                 }
+    //               }
+    //             ],
+    //             yAxes: [
+    //               {
+    //                 ticks: {
+    //                   min: 0
+    //                 }
+    //               }
+    //             ]
+    //           }
+    //     }
+    // })
 
     return(
         <div>
@@ -20,16 +100,7 @@ const StockChart = ({stock, chart}) => {
                 </span>
            </div>
            <div>
-                {chart.map((elem) => {
-                    return (
-                        <div>
-                            <p>{elem.symbol}</p>
-                            <p>{elem.date}</p>
-                            <p>Open: {elem.open}</p>
-                            <p>Close: {elem.close}</p>
-                        </div>
-                    )
-                })}
+                <canvas id='myLineChart' ref={chartRef} />
            </div>
         </div>
     )
