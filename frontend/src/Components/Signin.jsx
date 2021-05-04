820c3d2567277d0b79c4a3398f1a14af33671dcb
Owen Jones <owenthefirst93@gmail.com>
2021-05-03 21:18:13 -0400

    signin comp

--- frontend/src/Components/Signin.jsx
+++ frontend/src/Components/Signin.jsx
@@ -1,9 +1,43 @@
-import react from 'react'
+import react,{useState, useRef, useHistory, useAuth} from 'react'
 
 const Signin = () => {
+
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    let emailRef = useRef()
+    let passwordRef = useRef()
+
+    const { signin } = useAuth()
+    const history = useHistory()
+
+    const handleSignin = (e) => {
+        e.preventDefault()
+        setError('')
+        setLoading(true)
+
+        const email = emailRef.current.value
+        const password = passwordRef.current.value
+        
+        //push user page path onto history stack
+        signin(email,password)
+            .then((ref) => {
+                setLoading(false)
+                history.push('/')
+            })
+            .catch((err)=> {
+                setError(err.message)
+                setLoading(false)
+            })
+    }
     return (
         <div>
             <p>Signin</p>
+            <form onSubmit={(e)=> handleSignin(e)}>
+                <input type='text' ref={emailRef}></input>
+                <input type='text' ref={passwordRef}></input>
+                <button type='submit' disabled={loading}>Submit</button> 
+            </form>
         </div>
     )
 }
